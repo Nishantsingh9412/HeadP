184e4d34c39298525e98d72abeea489e2d81b0df
nishant <59526404+Nishantsingh9412@users.noreply.github.com>
2023-07-25 21:39:39 +0530

    initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,54 @@
+import {CgShoppingCart} from "react-icons/cg";
+import {AiOutlineHeart} from "react-icons/ai";
+import Search from "./Search/Search";
+import Cart from "../Cart/Cart";
+import { TbSearch } from "react-icons/tb";
+import { useState , useEffect ,useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { Context } from "../../utils/context";
+
+import "./Header.scss";
+
+
+const Header = () => {
+    const[scrolled,setScrolled] = useState(false);
+    const handleScroll = () =>{
+        const offset = window.scrollY;
+        console.log(offset);
+        if(offset>200){
+            setScrolled(true);
+        }else{
+            setScrolled(false);
+        }
+    };
+
+    useEffect(()=>{
+        // console.log('Hi UseEffect');
+        window.addEventListener("scroll",handleScroll);
+    },[])
+    
+    return (
+
+    <header className={`main-header ${scrolled? "sticky-header " : ""} `}> 
+        <div className="header-content">
+            <ul className="left">
+                <l1 className="list_items"> Home</l1> 
+                <l1 className="list_items"> About </l1>
+                <l1 className="list_items"> Categories </l1>   
+            </ul>
+            <div className="center"> JSBABA </div>
+            <div className="right">
+                <TbSearch />
+                <AiOutlineHeart />
+                <span className="cart-icon">
+                    <CgShoppingCart />
+                     <span>5</span>
+                </span>
+            </div>
+        </div>
+    </header>
+    );
+};
+
+export default Header;
+ 
