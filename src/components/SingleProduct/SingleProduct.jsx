8ea6e4f792dc190df81b9b846f8ff8e4b04c37db
nishant <59526404+Nishantsingh9412@users.noreply.github.com>
2023-07-26 16:20:12 +0530

    third commit

--- src/components/SingleProduct/SingleProduct.jsx
+++ src/components/SingleProduct/SingleProduct.jsx
@@ -1,6 +1,27 @@
-import "./SingleProduct.scss";
+import React from "react";
+import RelatedProducts from "./RelatedProducts/RelatedProducts";
+import { 
+    FaFacebook , 
+    FaTwitter , 
+    FaLinkedinIn ,
+    FaInstagram,
+    FaPinterest,
+    FaCartPlus
+} from 'react-icons/fa'
+
+// import prod from "../../assets/products/"
+import prod from "../../assets/products/earbuds-prod-2.webp" 
+
 const SingleProduct = () => {
-    return <div>Single Product</div>;
+    return <div className="single-product-main-content">
+        <div className="layout">
+            <div className="single-product-page">
+                    <img src={prod} alt="" />
+                <div className="left">Image </div>
+                <div className="right"> Prod Details </div>
+            </div>
+        </div>
+    </div>;
 };
 
 export default SingleProduct;
