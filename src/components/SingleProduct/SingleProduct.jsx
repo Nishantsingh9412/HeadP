2f1cb268e5b6278720c9dd2cb63f6600879e4aa1
nishant <59526404+Nishantsingh9412@users.noreply.github.com>
2023-07-27 15:24:06 +0530

    27th july commit

--- src/components/SingleProduct/SingleProduct.jsx
+++ src/components/SingleProduct/SingleProduct.jsx
@@ -16,10 +16,49 @@ const SingleProduct = () => {
     return <div className="single-product-main-content">
         <div className="layout">
             <div className="single-product-page">
-                    <img src={prod} alt="" />
-                <div className="left">Image </div>
-                <div className="right"> Prod Details </div>
+                <div className="left">
+                    <img src={prod} alt="" />    
+                </div>
+                <div className="right"> 
+                    <span className="name">  Product Name  </span>
+                    <span className="price">  Price </span>
+                    <span className="desc">  Product Description </span>
+
+                    <div className="cart-buttons">  
+                        <div className="quantity-buttons">
+                            <span>-</span>
+                            <span>5</span>
+                            <span>+</span>
+                        </div>
+                        <button className="add-to-cart-button">
+                        <FaCartPlus size={20} />
+                            ADD TO CART
+                        </button>
+                    </div>
+
+                    <span className="divider" />
+                    
+                    <div className="info-item">
+                        <span className="text-bold"> 
+                            Category:
+                            <span> Headphones </span> 
+                         </span>
+
+                         <span className="text-bold"> 
+                            Share:
+                            <span className="social-icons">
+                                <FaFacebook size={16} />
+                                <FaTwitter size={16} />
+                                <FaInstagram size={16} />
+                                <FaLinkedinIn size={16} />
+                                <FaPinterest size={16} />
+                            </span> 
+                         </span>
+                    </div>
+
+                 </div>
             </div>
+            <RelatedProducts /> 
         </div>
     </div>;
 };
