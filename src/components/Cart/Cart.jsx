f6a1f7cfb6d0674fab1ec828e920a8002f94755d
nishant <59526404+Nishantsingh9412@users.noreply.github.com>
2023-08-04 00:25:23 +0530

    4th aug commit

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { MdClose } from "react-icons/md";
 import { BsCartX } from "react-icons/bs";
 
+import CartItem from "./CartItem/CartItem";
+
 import "./Cart.scss";
 const Cart = ({setShowCart}) => {
     return (
@@ -15,11 +17,23 @@ const Cart = ({setShowCart}) => {
                         <span className="text"> Close </span>
                     </span>
                 </div>
-                <div className="empty-cart">
+                {/* <div className="empty-cart">
                      <BsCartX />
                      <span>No Products in the Cart. </span>
                      <button className="return-cta">  RETURN TO SHOP </button>
-                </div>
+                </div> */}
+                 <> {/* Empty Fragements */}
+                    <CartItem />
+                        <div className="cart-footer">
+                            <div className="subtotal">
+                                 <span className="text">SubTotal : </span>
+                                 <span className="text total">₹ 4999</span>
+                            </div>
+                                <div className="button">
+                                    <button className="checkout-cta">Checkout</button>
+                                </div>
+                        </div>
+                </>
             </div>
         </div>
     );
